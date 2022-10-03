4405a29b23f06aeaf264039425a44b622c480b61
C5m7b4 <mbedingfield@dcrpos.com>
2022-10-03 15:42:19 -0500

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+    port: 3007,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Vanilla js",
+      template: "public/index.html",
+    }),
+  ],
+  resolve: {
+    extensions: [".js"],
+  },
+};
