179c55919fd498c1d1cf92b430069b7026588ab6
C5m7b4 <mbedingfield@dcrpos.com>
2022-10-05 15:29:11 -0500

    add tests and ci

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -1,22 +1,25 @@
 import { isValid } from "../src/utils";
 
 describe("isValid", () => {
-  test("should return false for undefined", () => {
+  test("should return false if given an undefined value", () => {
     expect(isValid(undefined)).toEqual(false);
   });
-  test("shoudl return false for null values", () => {
+  test("should return false if given a null value", () => {
     expect(isValid(null)).toEqual(false);
   });
-  test("should return true for an int", () => {
+  test("should return true if given a number", () => {
     expect(isValid(1)).toEqual(true);
   });
-  test("should return true for a string", () => {
+  test("should return true if given a string", () => {
     expect(isValid("hello")).toEqual(true);
   });
-  test("should return true for an array", () => {
+  test("should return true if given an array", () => {
     expect(isValid([1, 2, 3])).toEqual(true);
   });
-  test("should return true for an object", () => {
+  test("should return true if given a valid object", () => {
     expect(isValid({ name: "mike" })).toEqual(true);
+  });
+  test("should return true if given a empty object", () => {
+    expect(isValid({})).toEqual(true);
   });
 });
